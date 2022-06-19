5a94f1da31cdf9deca9322b8ec4ce4b2d431d8f0
AlejandroHDZ2021 <alexhernandez.pro98@gmail.com>
2022-06-18 20:40:21 -0500

    quitando import

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { collection,addDoc } from 'firebase/firestore'
-import 'bootstrap/dist/css/bootstrap.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { db } from '../firebase'
